35584c0b00f32243fb4fc3c93b86a21efd0f8eb5
Rohan Bisht <rohan20ctbisht@gmail.com>
2025-05-21 13:00:21 +0530

    Fix formatting in user model: adjust indentation for isVerified field

--- Server/Models/user.model.js
+++ Server/Models/user.model.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    isVerified: {
+    isVerified: {     
         type: Boolean,
         default: false
     },
